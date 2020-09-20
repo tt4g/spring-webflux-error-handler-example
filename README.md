7c625dd46a3f3ea13ac4b3dc76a5712b4ac9b1ea
tt4g <mainlinett@gmail.com>
2020-09-20 17:08:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Overview
+
+Spring WebFlux error handling example.
+
+Official Reference: https://docs.spring.io/spring-boot/docs/2.3.4.RELEASE/reference/htmlsingle/#boot-features-webflux-error-handling
---
+++ src/main/java/com/github/tt4g/spring/webflux/error/handler/example/App.java
@@ -0,0 +1,14 @@
+package com.github.tt4g.spring.webflux.error.handler.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@SpringBootApplication
+@EnableWebFlux
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+
+}
