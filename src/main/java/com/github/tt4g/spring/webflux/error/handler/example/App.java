5de96848086ea30ddeaf901f8c150c2a00b22ca9
tt4g <45120617+tt4g@users.noreply.github.com>
2020-09-30 10:58:02 +0900

    Remove unnecessary annotation @EnableWebFlux
    if annotate @EnableWebFlux then WebFluxAutoConfiguration not working.
    Spring Boot application auto detect WebFlux.
    To force a web application type, set `spring.main.web-application-type=REACTIVE` property.
    
    https://docs.spring.io/spring-boot/docs/2.3.4.RELEASE/reference/htmlsingle/#boot-features-webflux-auto-configuration

--- src/main/java/com/github/tt4g/spring/webflux/error/handler/example/App.java
+++ src/main/java/com/github/tt4g/spring/webflux/error/handler/example/App.java
@@ -2,10 +2,8 @@ package com.github.tt4g.spring.webflux.error.handler.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
-@EnableWebFlux
 public class App {
 
     public static void main(String[] args) {
